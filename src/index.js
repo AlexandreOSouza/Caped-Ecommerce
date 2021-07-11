99337a3a5f8db6b83501d418222ea41e7f2cf7e0
Alexandre <alexandreSnow>
2021-07-11 00:06:04 -0300

    Create React App

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// app --> app.js, o reactDOM pega a classe e joga na div root do index.html
+ReactDOM.render(<App />, document.getElementById('root'));
